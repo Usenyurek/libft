3c4e739fc48d3f44b1e83c5a69744ee6ff52ba15
Usenyurek <utkucr0@gmail.com>
2025-06-06 17:04:14 +0300

    isalpha added

---
+++ ft_isalpha.c
@@ -0,0 +1,4 @@
+int ft_isalpha(int arg)
+{
+    return((arg <= 'z' && arg >= 'a') || (arg <= 'Z' && arg >= 'A'));
+}
--- libft.h
+++ libft.h
@@ -5,5 +5,6 @@ int             ft_strlen(char *str);
 int             ft_strncmp(char *s1, char *s2, unsigned int n);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 int             ft_atoi(char *str);
+char	*ft_strjoin(int size, char **strs, char *sep);
 
 #endif
